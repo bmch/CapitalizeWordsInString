ae8733f4c359e9e5e08abeea91a35beac4967f41
davstern <hello@davidstern.me>
2019-11-28 19:48:18 +0100

    pass

--- index.js
+++ index.js
@@ -6,5 +6,11 @@
 
 String.prototype.capitalizeWords = function() {
   //...
+  const arr = this.split(' ');
+  for(let i=0;i<arr.length;i++){
+    arr[i] = arr[i][0].toUpperCase()+ arr[i].slice(1).toLowerCase();
+  }
+  const ret = arr.join(' ');
+  return ret;
 };
 module.exports = String.prototype.capitalizeWords;
