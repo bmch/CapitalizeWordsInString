e55ca9a2c90b933065d6fffa5526f9587d411fdd
Gmiliuta <53524766+Gmiliuta@users.noreply.github.com>
2019-11-28 19:50:47 +0100

    done

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 
 // Refer to Test for Examples
 
-String.prototype.capitalizeWords = function() {
-  //...
+String.prototype.capitalizeWords = function(arg) {
+  const sentence = this.toString().split(' ');
+  for(let i =0; i < sentence.length; i++) {
+    sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].substring(1).toLowerCase();
+  }
+  return sentence.join(' ');
 };
 module.exports = String.prototype.capitalizeWords;
