b8e94711240a86818ca558e58b70709ce5cbb813
josefrangl <josefrangl@me.com>
2019-11-28 19:49:45 +0100

    solved one error in test

--- index.js
+++ index.js
@@ -5,6 +5,15 @@
 // Refer to Test for Examples
 
 String.prototype.capitalizeWords = function() {
-  //...
+  let arra = this.split(' ');
+  let capi = [];
+  arra.forEach(x => {
+    let begin = String(x).slice(0,1);
+    begin = begin.toUpperCase();
+    const string = begin + x.slice(1,);
+    console.log(string);
+    capi.push(string);
+  })
+  return capi.join(' ');
 };
 module.exports = String.prototype.capitalizeWords;
