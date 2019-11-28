94e16f78e405c1cb7941ddd4b98f3f935cf76995
Brendan McHugh <brendanmchugh12@gmail.com>
2019-11-28 14:43:46 +0100

    files added

---
+++ index.js
@@ -0,0 +1,11 @@
+// ### Challenge
+
+// GOOD LUCK!👍
+
+// Refer to Test for More Examples
+
+String.prototype.capitalizeWords = function() {
+  //...
+};
+
+module.exports = String.prototype.capitalizeWords;
---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('chai').should();
+
+var capitalizeWords = require('../index.js');
+
+describe('capitalizeWords', function() {
+  it('', function() {
+    dash(274).should.equal('2-7-4');
+  });
+});
